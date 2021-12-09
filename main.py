0aaf4c9b180e77b303e8fce9b67fca6ec23f63bd
sleepntsheep <sheep@papangkorn.com>
2021-12-09 18:19:39 +0700

    subprocess go brrrrrrrrrr

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import requests
 import sys
 from InquirerPy import inquirer
 import os
+import subprocess
 
 base_url: str = 'https://www1.gogoanime.cm'
+program: str = 'mpv'
 
 header = {
     'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36',
@@ -14,7 +16,6 @@ session = requests.session()
 
 def search(name: str) -> list:
     r: requests.Response = session.get(f'{base_url}//search.html', params = {'keyword': name}, headers=header)
-    # pattern = r'<a href="(/category/.+)" title="(.+)">.*<p class="name"'
     pattern: str = r'<div class="img">\s*<a href="/category/(.+)" title="(.+)">'
     matches: list = re.findall(pattern, r.text)
     d = {}
@@ -32,8 +33,7 @@ def get_episode_count(anime_id: str) -> int:
 
 def get_embed_link(anime_id: str, episode: int) -> str:
     r: requests.Response = requests.get(f'{base_url}/{anime_id}-episode-{episode}', headers=header)
-    # print(r.text)
-    pattern = r'''rel="100" data-video="(.*?)"\s?>'''
+    pattern = r'''data-video="(.*?embedplus\?.*?)"\s?>'''
     match = re.search(pattern, r.text)
     if match == None:
         return None
@@ -47,8 +47,6 @@ def get_link(embedded_link: str) -> str:
     return link
 
 def play_episode(anime_id: str, episode: int):
-   
-    
     embed_link: str = get_embed_link(anime_id, episode)
     if embed_link == None:
         return(print('Error: embed link not found'))
@@ -56,10 +54,12 @@ def play_episode(anime_id: str, episode: int):
     link: str = get_link(embed_link)
 
     print(f'Playing {anime_id}, episode {episode}')
+    print(f'{embed_link} {link}')
+    mpv = subprocess.Popen(f'mpv --http-header-fields="Referer: {embed_link}" {link}', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
-    os.system(f'mpv --http-header-fields="Referer: {embed_link}" {link}')
+    return mpv
 
-def search_cli(name=None):
+def get_anime_id(name=None):
     if name == None:
         name: str  = inquirer.text(message='Input anime title:').execute()
 
@@ -79,17 +79,16 @@ def search_cli(name=None):
     return anime_id
 
 def get_episode(ep_end):
-    episode: int = int(inquirer.text(message=F'Select episode to watch [1, {ep_end}]').execute())
+    episode: int = int(inquirer.text(message=F'Select episode to watch [1 - {ep_end}]').execute())
     while not (episode >= 1 and episode <= ep_end):
         episode: int = int(inquirer.text(message=F'Invalid episode, try again').execute())
-
+    return episode
 
 def main(name=None):
-
     if len(sys.argv) > 1:
-        anime_id = search_cli(' '.join(sys.argv[1:]))
+        anime_id = get_anime_id(' '.join(sys.argv[1:]))
     else:
-        anime_id = search_cli()
+        anime_id = get_anime_id()
 
     ep_start, ep_end = get_episode_count(anime_id)
 
@@ -100,7 +99,7 @@ def main(name=None):
     action = ''
     while action != 'Quit':
         action: str = inquirer.select(
-            message='Select anime to watch',
+            message='Select action',
             choices= [
                 'Replay the episode again',
                 'Select episode',
@@ -119,7 +118,7 @@ def main(name=None):
             else:
                 print('No more episode to watch')
         elif action == 'Search other anime':
-            anime_id = search_cli()
+            anime_id = get_anime_id()
             ep_end = get_episode_count(anime_id)
             play_episode(anime_id, get_episode(ep_end))
         elif action == 'Quit':
